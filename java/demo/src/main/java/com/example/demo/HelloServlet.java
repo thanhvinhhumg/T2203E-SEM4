3bd508f1a24aa75fc83395a852d4bba36c5406ef
thanhvinhhumg <thanhvinh200293@gmail.com>
2023-10-07 19:12:51 +0700

    init code

---
+++ java/demo/src/main/java/com/example/demo/HelloServlet.java
@@ -0,0 +1,49 @@
+package com.example.demo;
+
+import java.io.*;
+import java.util.Enumeration;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello T2203E!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        response.setContentType("text/html");
+
+        // Hello
+        String name = getInitParameter("name");
+        if (name != null) {
+            message = name;
+        }
+        Cookie[] cookies = request.getCookies();
+        Enumeration<String> headers = request.getHeaderNames();
+        String className =  request.getParameter("name");
+        PrintWriter out = response.getWriter();;
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("<h1>" + className + "</h1>");
+        out.println("<h1> Cookie </h1>");
+        for(int i= 0; i< cookies.length; i++) {
+            Cookie c = cookies[i];
+            out.println("<p> cookie name: " + c.getName() + "  value :"+ c.getValue()+ "</p>");
+        }
+        out.println("<h1> Header name and value </h1>");
+        while (headers.hasMoreElements()) {
+            String headerName = headers.nextElement();
+            String value = request.getHeader(headerName);
+            out.println("<p> header name: " + headerName + " header value :"+ value + "</p>");
+        }
+        out.println("</body></html>");
+    }
+    public void destroy() {
+        
+    }
+}
